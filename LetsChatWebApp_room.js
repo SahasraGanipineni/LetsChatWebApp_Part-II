72c3a8e89336eecf70577fcf503b47502654c775
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-10-02 12:18:57 +0530

    Add files via upload

---
+++ LetsChatWebApp_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5GxIxN4yc71Cgtf6BlVNoUA1Oeyi4tr8",
+    authDomain: "letschatwebapp-f9ca1.firebaseapp.com",
+    projectId: "letschatwebapp-f9ca1",
+    storageBucket: "letschatwebapp-f9ca1.appspot.com",
+    messagingSenderId: "675553109100",
+    appId: "1:675553109100:web:48d14527146e9c49be6449"
+  };
+
+  firebase.initializeApp(firebaseConfig);
